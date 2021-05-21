abbc90e6ba5a02c05d6f24e9a4766bfbee8bcc2d
Ibnu Aulia Nugraha Alihaq <ibnuauliana@gmail.com>
2021-05-22 01:18:04 +0900

    Auto show modal on first visit
    Auto show modal on first visit

--- index.php
+++ index.php
@@ -27,6 +27,34 @@
     <footer>
         <?php include ("module/footer/main.php"); ?>
     </footer>
+    <div class="modal fade" id="modal" tabindex="-1" role="dialog">
+        <div class="modal-dialog modal-lg" role="document">
+            <div class="modal-content">
+                <div class="modal-body">
+                    <p>One fine body&hellip;</p>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                    <button type="button" class="btn btn-primary">Save changes</button>
+                </div>
+            </div><!-- /.modal-content -->
+        </div><!-- /.modal-dialog -->
+    </div><!-- /.modal -->
 </html>
 <script type="text/javascript" src="js/jquery.js"></script>
 <script type="text/javascript" src="js/bootstrap.js"></script>
+<script>
+    $(document).ready(function() {
+        var dialogShown = localStorage.getItem('dialogShown')
+
+        if (!dialogShown) {
+            $(window).load(function(){
+                $( ".modal" ).modal('show');
+                localStorage.setItem('dialogShown', 1)
+            });
+        }
+        else {
+            $("#dialog1").hide();
+        }
+    });
+</script>
